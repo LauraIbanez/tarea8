42b1a42b05739f63dc8a8d5c20f35990780a04dc
LauraIbanez <laib9@hotmail.com>
2024-08-18 02:24:58 -0300

    puse funcion

--- Ejercicio fetch y json/js/script.js
+++ Ejercicio fetch y json/js/script.js
@@ -1,7 +1,7 @@
 // En este archivo no utilizamos el evento "DOMContentLoaded", ya que se colocó el atributo "defer" en la importación del script,
 // que nos soluciona el problema de los elementos no cargados del DOM. Más info => https://www.w3schools.com/tags/att_script_defer.asp
 
-const DATA_URL = "json/data.json"; // URL que contiene los datos que queremos mostrar
+const DATA_URL = "json/data.json" // URL que contiene los datos que queremos mostrar
 
 const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
 
@@ -18,4 +18,14 @@ function showData(dataArray) {
   }
 }
 
+  fetch (DATA_URL)
+  .then((resp) => resp.json())
+  .then(data => {
+    const students = data.students;
+    showData(students); 
+  })
+  .catch(function(error) {
+    console.log(error);
+    });
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
